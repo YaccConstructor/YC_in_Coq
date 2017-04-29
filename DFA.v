a118017d9a0753d938007f27f25764037d70113b
HeyLey <leila.xr@gmail.com>
2017-04-29 20:24:32 +0300

    add proof for lemma 2.1

--- DFA.v
+++ DFA.v
@@ -17,14 +17,14 @@ Record dfa { n : nat }: Type := mkDfa {
   next: (t n) -> ter -> (t n);
 }.
 
-Fixpoint final_state {n : nat} (d : dfa) (s: t n) (w: word) : t n :=
+Fixpoint final_state {n : nat} (next_d : (t n) -> ter -> (t n)) (s: t n) (w: word) : t n :=
    match w with
      | nil => s
-     | h :: t => final_state d (next d s h) t
+     | h :: t => final_state next_d (next_d s h) t
   end.
 
 Definition accepts (n : nat) (d : dfa) (s: t n) (w: word) : Prop :=
-  In (final_state d s w) (final d).
+  In (final_state (next d) s w) (final d).
 
 
 (** The type of deterministic finite automata. ***)
@@ -47,16 +47,16 @@ Definition dfa_language {n : nat} (d : dfa (n:=n)) := (accepts n d (start d)).
 
 Definition s_dfa_language {n : nat} (d : s_dfa (n:=n)) := (s_accepts d (s_start d)).
 
-Definition create_s_dfa {n : nat} (d : dfa (n:=n)) (h : t n) := s_mkDfa n (start d) h (next d).
+Definition create_s_dfa (n : nat) (st_d : t n) (next_d : (t n) -> ter -> (t n)) (h : t n) := s_mkDfa n st_d h next_d.
 
-Fixpoint split_dfa_list {n : nat} (d : dfa) (f_list : list (t n))
+Fixpoint split_dfa_list {n : nat}  (st_d : t n) (next_d : (t n) -> ter -> (t n)) (f_list : list (t n))
    : list s_dfa :=
   match f_list with
      | nil => nil
-     | h :: t =>  create_s_dfa d h :: split_dfa_list d t
+     | h :: t =>  create_s_dfa n st_d next_d h :: split_dfa_list st_d next_d t
   end.
 
-Definition split_dfa {n : nat} (d: dfa (n:=n)) := split_dfa_list d (final d).
+Definition split_dfa {n : nat} (d: dfa (n:=n)) := split_dfa_list (start d) (next d) (final d).
 
 Definition language_union (l1 l2 : language) := fun w => (l1 w) \/ (l2 w).
 
@@ -201,15 +201,15 @@ Proof.
   exact H.
 Qed.
 
-Theorem T0: forall (n:nat) (d: dfa (n:=n)) (st : t n) (e : t n) (w: word),
-  (final_state d st w) =
-  (s_final_state (create_s_dfa d e) st w).
+
+Theorem T0: forall (n:nat) (start0: t n) (final0 : list (t n)) (next0 : t n -> ter -> t n) (st : t n) (e : t n) (w: word),
+  (final_state next0 st w) =
+  (s_final_state (create_s_dfa n start0 next0 e) st w).
 Proof.
-  intros n d st e w.
+  intros.
   revert st.
-  destruct d.
   simpl.
-  unfold create_s_dfa. simpl.
+  unfold create_s_dfa.
   induction w.
   simpl.
   reflexivity.
@@ -218,11 +218,77 @@ Proof.
   apply IHw.
 Qed.
 
-Theorem T21: forall d : dfa, language_eq (dfa_language d) (language_list_union (map (s_dfa_language) (split_dfa d))).
+
+Theorem T21_1: forall (n : nat) (d : dfa (n:=n)) (w : word),
+    dfa_language d w -> language_list_union (map s_dfa_language (split_dfa d)) w.
+Proof.
+  intros n d w.
+  destruct d.
+  set (d := {| start := start0; final := final0; next := next0 |}).
+  intro H1.
+  unfold split_dfa.
+  simpl.
+  unfold dfa_language in H1.
+  simpl in H1.
+  unfold accepts in H1.
+  simpl in H1.
+  induction final0.
+  simpl in H1.
+  elim H1.
+  simpl in H1.
+  destruct H1.
+  simpl.
+  left.
+  unfold s_dfa_language.
+  simpl.
+  unfold s_accepts.
+  simpl.
+  rewrite <- T0.
+  auto.
+  auto.
+  simpl.
+  right.
+  apply IHfinal0.
+  auto.
+Qed.
+
+
+Theorem T21_2: forall (n : nat) (d : dfa (n:=n)) (w : word),
+    language_list_union (map s_dfa_language (split_dfa d)) w -> dfa_language d w.
+Proof.
+  intros n d w.
+  destruct d.
+  set (d := {| start := start0; final := final0; next := next0 |}).
+  unfold split_dfa.
+  simpl.
+  unfold dfa_language.
+  simpl.
+  unfold accepts.
+  simpl.
+  intro H1.
+  induction final0.
+  auto.
+  simpl in H1.
+  simpl.
+  destruct H1.
+  left.
+  unfold s_dfa_language in H.
+  simpl in H.
+  unfold s_accepts in H.
+  simpl in H.
+  rewrite T0 with (start0:=start0) (e:=a).
+  auto.
+  auto.
+  right.
+  apply IHfinal0.
+  auto.
+Qed.
+
+
+Theorem T21:  forall (n : nat) (d : dfa (n:=n)), language_eq (dfa_language d) (language_list_union (map (s_dfa_language) (split_dfa d))).
 Proof.
   intros.
   apply mk_laguage_eq.
-  intros w H1.
-  unfold dfa_language in H1.
-  unfold accepts in H1.
-  unfold split_dfa.
+  apply T21_1.
+  apply T21_2.
+Qed.
